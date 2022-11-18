e6bf2d33ea7f6c77d885c2ea8cb402fbb14e7b05
delguanzon <yodelguanzon@gmail.com>
2022-11-18 14:03:27 -0800

    Display current value in the form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import './assets/css/styles.css';
 import ForexService from './../src/js/forexservice.js';
 
-window.addEventListener('load', function () {
 
-console.log(`${process.env.API_KEY}`);
-let response = ForexService.getForexUsd();
-console.log(`${response}`);
+//Business Logic
+async function getForexUSD() {
+  const response = await ForexService.getForexUsd();
+  if (response.conversion_rates) {
+    printConversion(response);
+  } else {
+    console.log(response);
+  }
+}
+
+
+function printConversion(response) {
+  document.getElementById("forexAmount").value = response.conversion_rates.PHP;
+}
+
+function handleFormSubmission(e) {
+  e.preventDefault();
+  getForexUSD();
+
+}
+
+//UI Logic
+window.addEventListener('load', function () {
+  document.getElementById('form').addEventListener('submit', handleFormSubmission);
+  console.log(`${process.env.API_KEY}`);
+  let response = ForexService.getForexUsd();
+  console.log(`${response}`);
 });
