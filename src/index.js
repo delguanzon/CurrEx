5e3e3f38a0af03dd27153ecfeb7e6c872c5ebc78
delguanzon <yodelguanzon@gmail.com>
2022-11-18 12:38:32 -0800

    Create ForexService

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/styles.css';
+import ForexService from './../src/js/forexservice,js';
+
 window.addEventListener('load', function () {
 console.log(`${process.env.API_KEY}`);
 });
---
+++ src/js/forexservice.js
@@ -0,0 +1,20 @@
+class ForexService {
+  static async getForexUsd() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const jsonResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonResponse;
+    }
+    catch (error) {
+      return error;
+    }
+  }
+
+  // static async getPairEx() {
+
+  // }
+}
